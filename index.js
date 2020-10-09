be1673240bb868b649a9188e4bbf4389cf40a720
william jacobs <williamjacobs@williams-MacBook-Pro.local>
2020-10-08 19:14:47 -0700

    created routes to save notes to database

--- index.js
+++ index.js
@@ -1,14 +1,46 @@
 const path = require('path')
+const fs = require('fs')
 const express = require('express')
 const app = express()
 
 const port = process.env.PORT || 3000
-
+app.use(express.json())
+app.use(express.urlencoded())
 app.use(express.static(path.join(__dirname, "public")))
+
+app.get("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+        if (err) {
+            res.send("you have an error")
+            return
+        }
+        res.json(JSON.parse(data))
+    })
+})
+
+app.post("/api/notes", (req, res) => {
+    fs.readFile(path.join(__dirname, "db/db.json"), "utf8", (err, data) => {
+        if (err) {
+            res.send("you have an error")
+            return
+        }
+        const notes = JSON.parse(data)
+        console.log(req.body)
+        notes.push(req.body)
+        fs.writeFile(path.join(__dirname, "db/db.json"), JSON.stringify(notes), "utf8", (err1) => {
+            if (err1) {
+                res.send("you have an error")
+                return
+            }
+            res.json(notes)
+        })
+    })
+})
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "public/notes.html"))
 })
+
 app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public/index.html"))
 })
